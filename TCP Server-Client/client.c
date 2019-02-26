c7e12d76fe0b7b028fb785de1707b80fbfb27ac0
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2019-02-26 17:16:09 +0530

    Commit Assignment 1.

---
+++ TCP Server-Client/client.c
@@ -0,0 +1,105 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define PORT 8080
+#define MAX_SIZE 1024
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr; // Special data structure to hold address of server. 
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    /* Creating a socket for the client to communicate through.*/
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr)); // to make sure the struct is empty. Essentially sets sin_zero as 0
+                                                // which is meant to be, and rest is defined below
+
+    serv_addr.sin_family = AF_INET; // Refers to addresses from internet, IP addresses specifically.
+    serv_addr.sin_port = htons(PORT); // Specifies the port through which it is going to connect to server. htons converts to Big Endian - Left to Right. RTL is Little Endian
+
+    // Converts an IP address in numbers-and-dots notation into either a
+    // struct in_addr or a struct in6_addr depending on whether you specify AF_INET or AF_INET6.
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)  // connect to the server address
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    printf("Welcome to the client!\n");
+    // Runs an infinite loop to keep taking commands from the user, until exit is entered.
+    while(1){
+        printf(">>");
+        char buffer[1024];
+        char store[1024];
+        for(int i = 0; i < 1024; ++i)
+            buffer[i] = '\0';
+        /* Accepting input from the user. */
+        int len = 0;
+        char c;
+        scanf("%c", &c);
+        buffer[len++] = c;
+        while(c != '\n')
+        {
+            scanf("%c", &c);
+            buffer[len++] = c;
+        }
+        buffer[len - 1] = '\0';
+        for(int i = 0; i < 1024; ++i)
+            store[i] = '\0';
+        for(int i = 0; i < len; ++i)
+            store[i] = buffer[i];
+        /**********************************/
+        
+        send(sock , buffer , strlen(buffer) , 0 );  // send the message.
+        
+        for(int i = 0; i < 1024; ++i)
+            buffer[i] = '\0';
+        
+        valread = read( sock , buffer, 1024);  // receive message back from server, into the buffer
+        
+        /* Processing according to message from server*/
+        if(strcmp(buffer, "Thanks for connecting! Have a nice Day!") == 0){
+            /* Exit Client */
+            printf("%s\n",buffer);    
+            break;
+        }
+        if(strcmp(buffer, "What file do you want from the server??") == 0){
+            /* Handles the case where user enters send but forgets to enter what file he wants */
+            printf("%s\n", buffer);
+            continue;
+        }
+        if(strcmp(buffer, "File Not found!") == 0){
+            /* Handles the case where user requests a missing file. */
+            printf("%s\n", buffer);
+            continue;
+        }
+        char * token = strtok(store, " "); /* Used for tokenizing. token now contains first token of store. Tokens are separated by space character */
+        if(strcmp(token, "send") == 0){
+            token = strtok(NULL, " "); /* Get next token i.e second token of store */
+            FILE *f = fopen(token, "w"); // Opens a file with the name as the one requested for.
+            fprintf(f, "%s", buffer); // Write the response from the server to this file.
+            fclose(f); // Close file buffer
+            printf("File downloaded successfully!\n");
+            continue;
+        }
+        // Corresponds to listall command output
+        printf("%s\n", buffer);
+    }
+    return 0;
+}
---
+++ TCP Server-Client/server.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+#include <dirent.h>
+#define PORT 8080
+#define MAX_SIZE 1024
+
+void * socketThread(void * arg)
+{
+    /* This code segment is shared by each client thread. */
+    int valread;
+    char buffer[1024] = {0};
+    char *response = "Hello World!\n";
+    int new_socket = *((int *)arg);
+    // Infinite loop to keep listening to port for data from client.
+    while(1){
+        for(int i = 0; i < 1024; ++i)
+            buffer[i] = '\0';
+        valread = read( new_socket , buffer, 1024);  // read information from client
+        
+        /* Some processing done to extract first word separated by default delimiter white space. */
+        char word[1024] = {0};
+        int b = 0;
+        while(buffer[b] != ' ' && buffer[b] != '\0')
+        {
+            word[b] = buffer[b];
+            b++;
+        }
+        word[b] = '\0';
+        /*******************************************************************************************/
+        
+        if(strcmp(buffer, "listall") == 0)
+        {
+            // Received command listall from client
+            char ls[1024];
+            int c = 0;
+            DIR *dir_name;
+            struct dirent *dir; // Special data structure to hold details of files in directories.
+            dir_name = opendir("."); // Open current directory
+            if(dir_name)
+            {
+                /* Keeps reading the contents of directory and puts them in ls character array */
+                while((dir = readdir(dir_name)) != NULL)
+                {
+                    for(int j = 0; j < strlen(dir->d_name); ++j)
+                        ls[c++] = dir->d_name[j];
+                    ls[c++] = '\n';
+                }
+                ls[c - 1] = '\0';
+                closedir(dir_name); // Close directory
+            }
+            response = &ls[0];
+        }
+        else if(strcmp(buffer, "exit") == 0){
+            /* Special response to client if it wants to disconnect*/
+            response = "Thanks for connecting! Have a nice Day!";
+            send(new_socket , response , strlen(response) , 0 );  // use sendto() and recvfrom() for DGRAM
+            printf("Response sent to client!\n");
+            break; // Break out of infinite loop as client wants to disconnect
+        }
+        else if(strcmp(word, "send") == 0){
+            /* This is the case when a user requests a file from the server. */
+            char * token = strtok(buffer, " ");
+            token = strtok(NULL, " ");
+            /* If only one token, user has forgot to send file name */
+            if(token == NULL){
+                response = "What file do you want from the server??";
+            }
+            else{
+                /* Checks for the existence of the file in the current directory */
+                DIR *dir_name;
+                struct dirent *dir;
+                dir_name = opendir(".");
+                int found = 0;
+                if(dir_name)
+                {
+                    while((dir = readdir(dir_name)) != NULL)
+                    {
+                        if(strcmp(dir->d_name, token) == 0)
+                        {
+                            found = 1;
+                        }
+                    }
+                    closedir(dir_name);
+                }
+                /*****************************************************************/
+                if(found){
+                    // If requested file is a valid file...
+                    FILE * file;
+                    file = fopen(token, "r"); // Open file
+                    char response_data[MAX_SIZE];
+                    /* Read the contents of the opened file into character array */
+                    for(int i = 0; i < MAX_SIZE; ++i)
+                        response_data[i] = '\0';
+                    char ch;
+                    int r = 0;
+                    while((ch = fgetc(file)) != EOF && r < (MAX_SIZE - 1))
+                        response_data[r++] = ch;
+                    /************************************************************/
+                    
+                    response = &response_data[0]; // Sets the response variable with value of response_data character array.
+                }
+                else{
+                    // Client has requested an invalid file.
+                    response = "File Not found!";
+                }
+            }
+        }
+        else{
+            // Dealing with invalid commands
+            response = "Invalid Command!";
+        }
+        send(new_socket , response , strlen(response) , 0 );  // use sendto() and recvfrom() for DGRAM
+        printf("Response sent to client!\n");
+    }
+    pthread_exit(NULL); // exits the current thread so that the parent knows.
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;  // sockaddr_in - references elements of the socket address. "in" for internet
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  // creates socket, SOCK_STREAM is for TCP. SOCK_DGRAM for UDP
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // This is to lose the pesky "Address already in use" error message
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt))) // SOL_SOCKET is the socket layer itself
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;  // Address family. For IPv6, it's AF_INET6. 29 others exist like AF_UNIX etc.
+    address.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any IP address - listens from all interfaces.
+    address.sin_port = htons( PORT );    // Server port to open. Htons converts to Big Endian - Left to Right. RTL is Little Endian
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Port bind is done. You want to wait for incoming connections and handle them in some way.
+    // The process is two step: first you listen(), then you accept()
+    if (listen(server_fd, 3) < 0) // 3 is the maximum size of queue - connections you haven't accepted
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    pthread_t thread[105];
+    int cnt = 0;
+    while(1){
+        // returns a brand new socket file descriptor to use for this single accepted connection. Once done, use send and recv
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        if(new_socket < 0)
+        {
+            perror("accept");
+            continue;
+        }
+        // Whenever a new client requests connection, create a thread. The thread starts execution from socketThread routine defined above.
+        /* A thread is a light weight process, which shares same code segment, global variables etc but has its own local stack,etc. */
+        int ret = pthread_create(&thread[cnt++], NULL, socketThread, &new_socket);
+        if(ret != 0)
+        {
+            perror("Failed to create thread");
+            continue;
+        }
+    }
+    return 0;
+}
