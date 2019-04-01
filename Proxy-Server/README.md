02099cfad1f57b9b2e99aca489390f7c4c629e03
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2019-04-02 02:56:22 +0530

    Add Basic Access Authentication.

--- Proxy-Server/README.md
+++ Proxy-Server/README.md
@@ -1,1 +1,3 @@
 curl --request GET --proxy 127.0.0.1:8080 --local-port 20001 127.0.0.1:8082/1.data
+
+curl --request GET -u username:password --proxy 127.0.0.1:20000 --local-port 20001-20010 127.0.0.1:19998/1.data
--- Proxy-Server/proxy.cpp
+++ Proxy-Server/proxy.cpp
@@ -20,10 +20,147 @@ struct element{
     string client_ip;
     int destination_port;
     int client_port;
+    bool isAuthenticated;
     string method;
     string filename;
 };
 
+/**************************base64 encode and decode functions*************************************/
+/* 
+   base64.cpp and base64.h
+
+   Copyright (C) 2004-2008 René Nyffenegger
+
+   This source code is provided 'as-is', without any express or implied
+   warranty. In no event will the author be held liable for any damages
+   arising from the use of this software.
+
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+
+   1. The origin of this source code must not be misrepresented; you must not
+      claim that you wrote the original source code. If you use this source code
+      in a product, an acknowledgment in the product documentation would be
+      appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+      misrepresented as being the original source code.
+
+   3. This notice may not be removed or altered from any source distribution.
+
+   René Nyffenegger rene.nyffenegger@adp-gmbh.ch
+
+*/
+
+static const std::string base64_chars = 
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
+
+static inline bool is_base64(unsigned char c) {
+  return (isalnum(c) || (c == '+') || (c == '/'));
+}
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
+
+    while((i++ < 3))
+      ret += '=';
+
+  }
+
+  return ret;
+
+}
+std::string base64_decode(std::string const& encoded_string) {
+  int in_len = encoded_string.size();
+  int i = 0;
+  int j = 0;
+  int in_ = 0;
+  unsigned char char_array_4[4], char_array_3[3];
+  std::string ret;
+
+  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+    char_array_4[i++] = encoded_string[in_]; in_++;
+    if (i ==4) {
+      for (i = 0; i <4; i++)
+        char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+      for (i = 0; (i < 3); i++)
+        ret += char_array_3[i];
+      i = 0;
+    }
+  }
+
+  if (i) {
+    for (j = i; j <4; j++)
+      char_array_4[j] = 0;
+
+    for (j = 0; j <4; j++)
+      char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+  }
+
+  return ret;
+}
+/******************************************************************************************/
+bool authenticate(string S){
+    bool success = false;
+    ifstream file("username_password.txt");
+    if(file.is_open()){
+        string line;
+        while(getline(file, line)){
+            if(line == S){
+                success = true;
+            }
+        }
+        file.close();
+    }
+    return success;
+}
+/******************************************************************************************/
 void parse(string http_request, element * ptr){
     string dest_ip;
     string dest_port;
@@ -49,6 +186,11 @@ void parse(string http_request, element * ptr){
 	ptr -> destination_ip = dest_ip;
 	ptr -> filename = filenm;	
 	ptr -> method = get_post;
+    string auth = "";
+    int idx = http_request.find("Authorization: Basic ") + 21;
+    for(int i = idx; http_request[i] != '\n'; ++i)
+        auth += http_request[i];
+    ptr->isAuthenticated = authenticate(base64_decode(auth));
     return;
 }
 
@@ -122,7 +264,7 @@ void * serveRequest(void * arg)
     for(int i = 0; i < strlen(buffer); ++i)
         http_request = http_request + buffer[i];
     /****************************************************************************************/
-
+    //cout << base64_decode("dXNlcm5hbWU6cGFzc3dvcmQ=") << endl;
     /********************************* Parse HTTP request ***********************************/
     parse(http_request, ptr);
     /****************************************************************************************/
